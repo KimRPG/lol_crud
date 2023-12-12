2ee11ff33e277eaeb53f78aab4a615b1d5e0984d
Kim-Hyun-Seung <120882082+Kim-Hyun-Seung@users.noreply.github.com>
2023-12-12 11:39:53 +0900

    controller

--- src/main/java/com/character/lol/controller/LoLController.java
+++ src/main/java/com/character/lol/controller/LoLController.java
@@ -4,30 +4,40 @@ import com.character.lol.dto.CharacterDto;
 import com.character.lol.service.LoLService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 public class LoLController {
     @Autowired
     LoLService service;
 
-    @GetMapping("/add")
-    public String add() {
-        return "addView";
-    }
     @PostMapping("/create")
-    public ResponseEntity create(CharacterDto dto) {
+    public ResponseEntity create(@RequestBody CharacterDto dto) {
         ResponseEntity result = service.create(dto);
         return result;
     }
 
     @GetMapping("/readall")
-    public ResponseEntity readall(Model model) {
+    public ResponseEntity readall() {
         ResponseEntity response = service.readAll();
-        model.addAttribute(response);
+        return response;
+    }
+    @GetMapping("/read/{name}")
+    public ResponseEntity read(@PathVariable String name){
+        ResponseEntity response = service.readOne(name);
         return response;
     }
 
+    @DeleteMapping("/delete/{name}")
+    public ResponseEntity delete(@PathVariable String name){
+        ResponseEntity result = service.delete(name);
+        return result;
+    }
+
+    @PutMapping("/update/{name}")
+    public ResponseEntity update(@PathVariable String name, @RequestBody CharacterDto dto){
+        ResponseEntity result = service.update(dto, name);
+        return result;
+    }
 }
